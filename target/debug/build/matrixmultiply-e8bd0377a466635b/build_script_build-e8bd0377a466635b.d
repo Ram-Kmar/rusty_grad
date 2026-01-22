0ca9eb02fd89bca3a0c1910fee2d10abfa151ecf
ramkumar <ramkumar@ramkumars-Mac-mini.local>
2026-01-22 16:17:47 +0530

    chore: remove build artifacts and system files from tracking

--- target/debug/build/matrixmultiply-e8bd0377a466635b/build_script_build-e8bd0377a466635b.d
+++
@@ -1,5 +0,0 @@
-/Users/ramkumar/dev/rusty_grad/target/debug/build/matrixmultiply-e8bd0377a466635b/build_script_build-e8bd0377a466635b.d: /Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/matrixmultiply-0.3.10/build.rs
-
-/Users/ramkumar/dev/rusty_grad/target/debug/build/matrixmultiply-e8bd0377a466635b/build_script_build-e8bd0377a466635b: /Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/matrixmultiply-0.3.10/build.rs
-
-/Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/matrixmultiply-0.3.10/build.rs:
--- target/debug/deps/rawpointer-758af260305723ec.d
+++
@@ -1,7 +0,0 @@
-/Users/ramkumar/dev/rusty_grad/target/debug/deps/rawpointer-758af260305723ec.d: /Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/Users/ramkumar/dev/rusty_grad/target/debug/deps/librawpointer-758af260305723ec.rlib: /Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/Users/ramkumar/dev/rusty_grad/target/debug/deps/librawpointer-758af260305723ec.rmeta: /Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs
-
-/Users/ramkumar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rawpointer-0.2.1/src/lib.rs:
